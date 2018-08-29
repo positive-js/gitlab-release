a3fe2a9a22e6a4632cae01e0563e6d99bcd91a77
Oleg Pimenov <pimenovo@gmail.com>
2018-08-29 20:57:20 +0300

    feat: added verbose log

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require('path');
+const fs = require('fs');
 const _ = require('lodash');
 
 const Listr = require('listr');
+const VerboseRenderer = require('listr-verbose-renderer');
 const Bluebird = require('bluebird');
 const latestSemverTag = Bluebird.promisify(require('git-latest-semver-tag'));
+const recommendedBump = Bluebird.promisify(require('conventional-recommended-bump'));
 const streamToArray = require('stream-to-array');
 const rawCommitsStream = require('git-raw-commits');
 
@@ -59,30 +63,90 @@ function gitlabRelease(packageOpts, { logger }) {
 
     config.buildType = (packageOpts.buildType) ? buildTypes[packageOpts.buildType] : buildTypes[branchType[0]];
 
-    const tasks = new Listr([]);
+    const tasks = new Listr([], { renderer: VerboseRenderer });
 
     tasks.add([
         {
-            title: 'Latest Semver Tag',
-            task: (ctx, task) => {
-                task.output = ''
-                return latestSemverTag()
-                    .then(latestTag => {
-                        return streamToArray(rawCommitsStream({from: latestTag}));
-                    })
+            title: 'Get Commits From Latest Semver Tag',
+            task: (ctx, task) => new Promise((resolve, reject) => {
+
+                latestSemverTag()
+                    .then(latestTag => streamToArray(rawCommitsStream({from: latestTag})))
                     .then(_.partial(_.map, _, value => value.toString()))
                     .then((commits) => {
-                        logger.log(`commit messages from last tag: %O`, commits);
-                    });
-            }
+                        if (commits.length === 0) {
+                            task.output = `no commits to release so skipping the other release steps`;
+                            reject();
+                            return;
+                        }
+
+                        task.output = commits;
+                        ctx.commits = commits;
+                        resolve();
+                    })
+                    .catch(err => {
+                        reject(err);
+                    })
+
+            })
         },
         {
-            title: 'Commit Messages',
-            task: (ctx) => {
+            title: 'Get Recommended Bump',
+            task: (ctx, task) => new Promise((resolve, reject) => {
 
-                return logger.log(`commit messages from last tag: %O`);
-            }
+                recommendedBump({ ignoreReverted: false, preset: config.options.preset })
+                    .then((recommendation) => {
+                        task.output = `recommended version bump is - ${JSON.stringify(recommendation)}`;
+
+                        if (recommendation.releaseType === undefined) {
+                            task.output = `no recommended release so skipping the other release steps`;
+                            reject();
+                        }
+                    })
+                    .catch(err => {
+                        reject(err);
+                    })
+            })
         },
+        {
+            title: 'Increment package.json version',
+            task: (ctx, task) => new Promise((resolve, reject) => {
+
+                let nextVersion;
+
+                if (config.buildType().isRelease) {
+
+                    nextVersion = semver.inc(config.pkg.version, 'patch');
+                } else if (config.buildType().ifFeature) {
+
+                    const featureName = branchType[1].split(/_(.+)/)[1];
+
+                    nextVersion = semver.inc(config.pkg.version, 'prerelease', featureName);
+                } else if (config.buildType().isFourDigits) {
+
+                    nextVersion = semver.inc(config.pkg.version, 'prerelease', 'build');
+                } else {
+                    task.output = 'incPkgVersion: unsupported buildType';
+                    reject();
+                }
+
+                new Promise((resolve, reject) => {
+                    const out = fs.createWriteStream(path.join(process.cwd(), `package.json`))
+                        .on('finish', resolve)
+                        .on('error', reject);
+
+                    out.write(JSON.stringify(config.pkg, null, '    '));
+                    out.end();
+                })
+                    .then(() => {
+
+                        resolve();
+                    })
+                    .catch(() => {
+                        reject();
+                    })
+            })
+        }
     ]);
 
     return tasks.run();
